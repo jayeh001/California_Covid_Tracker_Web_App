25bc5edb58ae014cc443a964d2f6e54225455e34
Anthony Morris <myThinkTank@anthonys-mbp.myfiosgateway.com>
2022-11-21 16:57:24 -0500

    Added the required files for the server.

---
+++ server/README.md
@@ -0,0 +1,23 @@
+# CIS550 Group Project Server Implementation
+
+This is th server implementation for the CIS550 Group Project. This server provides the backend functionality for the application. Specifically, the API routes and DB connections are handled by the server.
+
+## Initialize the environment
+
+To initialize the server, run the following command:
+
+```
+npm install
+```
+
+## Starting the server
+
+To start the server, run the following command
+
+```
+node server.js
+```
+
+## Testing the server
+
+To test the server, open a web browser and navigate to: http://127.0.0.1:8080/hello
---
+++ server/routes.js
@@ -0,0 +1,32 @@
+const config = require('./config.json')
+const mysql = require('mysql');
+const e = require('express');
+
+// TODO: fill in your connection details here
+const connection = mysql.createConnection({
+    host: config.rds_host,
+    user: config.rds_user,
+    password: config.rds_password,
+    port: config.rds_port,
+    database: config.rds_db
+});
+connection.connect();
+
+
+// ********************************************
+//            SIMPLE ROUTE EXAMPLE
+// ********************************************
+
+// Route 1 (handler)
+async function hello(req, res) {
+    // a GET request to /hello?name=Steve
+    if (req.query.name) {
+        res.send(`Hello, ${req.query.name}! Welcome to the CIS5500 Group Project server!`)
+    } else {
+        res.send(`Hello! Welcome to the cis550-final-project server!`)
+    }
+}
+
+module.exports = {
+    hello,
+}

06e9e0f4d4f7ff0eeac0e01d1d36743e9d6138fe
Si Miao Zhang <95888591+sz125@users.noreply.github.com>
2022-11-29 18:19:03 -0800

    initial fetcher file (followed HW2 code)

---
+++ client/cis550-group-project/src/fetcher.js
@@ -0,0 +1,31 @@
+import config from './config.json'
+
+const getCovid = async (type, county_name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/covid/${type}?fips=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getCorrelations = async (category, county_name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/correlations/${category}?fips=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getTimeline = async (type, county_name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/timeline/${type}?fips=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+export {
+    getAllMatches,
+    getAllPlayers,
+    getMatch,
+    getPlayer,
+    getMatchSearch,
+    getPlayerSearch
+}

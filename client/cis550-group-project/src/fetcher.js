14da0e7d9cb47e12ca5270326942748074c2bac6
Si Miao Zhang <95888591+sz125@users.noreply.github.com>
2022-12-01 16:59:20 -0800

    Create fetcher.js

---
+++ client/cis550-group-project/src/fetcher.js
@@ -0,0 +1,28 @@
+import config from './config.json'
+
+const getCovid = async (county_name, type) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/covid/${type}?county_name=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getCorrelations = async (category, county_name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/correlations/${category}?fips=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getTimeline = async (type, county_name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/timeline/${type}?fips=${county_name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+export {
+    getCovid,
+    getCorrelations,
+    getTimeline
+}

ae4765f2ed694b2e50ea36808548dfa978b14b04
Anthony Morris <myThinkTank@anthonys-mbp.myfiosgateway.com>
2022-12-06 00:12:36 -0500

    Updated the CovidPage

--- client/cis550-group-project/src/pages/CovidPage.js
+++ client/cis550-group-project/src/pages/CovidPage.js
@@ -8,46 +8,46 @@ import { useState } from "react";
 const CovidPage = () => {
 
 	const [covidData, setCovidData] = useState();
-	const [county, setCounty] = useState();
+	const [county, setCounty] = useState('Alameda');
 	const [queryType, setQueryType] = useState();
 
 
-    useEffect(() => {
-        getCovid('Amador', 'cases').then(res => {
+	useEffect(() => {
+        getCovid('Alameda', 'cases').then(res => {
             setCovidData(res.results)
         })
     }, [])
 
 
 	function onChangeCounty(event) {
-		event.preventDefault();
 		console.log('the value is: ' + event.target.value)
-
-        setCounty(event.target.value)
-        console.log('county changed to: ' + county)
-        getCovid(county, 'cases').then(res => {
+		setCounty(event.target.value)
+		console.log('county changed to: ' + county)
+		getCovid(event.target.value, queryType).then(res => {
             setCovidData(res.results)
-        })
+		})
+		
 	}
 	
 	function onChangeQueryType(event) {
-		event.preventDefault();
 		console.log(event.target.value)
-		
-        // var countyName = event.target.value
-        // console.log(event.target.value);
-       /* getCovid(countyName, 'cases').then(res => {
+		setQueryType(event.target.value)
+		console.log(queryType)
+       getCovid(county, event.target.value).then(res => {
             setCovidData(res.results)
         })
-        console.log('changing')*/
+        console.log('changing')
     }
 
+	// Don't worry about this reloading twice. It has no effect on the rendering, which
+	// is what actually counts. Specifically, the JSX in the return block below.
     console.log(covidData)
 
 
 	return (
 		<>
-			<div>
+			<form>
+				<div>
 				<select value={county} onChange={onChangeCounty}>
 					<option value="Alameda">Alameda</option>
 					<option value="Amador">Amador</option>
@@ -59,26 +59,15 @@ const CovidPage = () => {
                     <option value="correlation">Correlation</option>
 				</select>
 			</div>
-
+			</form>
+			<div>
+				{<p>{covidData && covidData[0].cases_per_100k ? covidData[0].cases_per_100k : null}</p>}
+				{<p>{covidData && covidData[0].Correlation ? covidData[0].Correlation : null}</p>}
+				{county && <p>{county}</p>}
+			</div>
+			
 		</>
 		
-        // <div>
-        //     <h1>Covid Page</h1>
-        //     <form action='/covid'>
-        //         <label for="county"><b>County:</b> &nbsp;</label>
-        //         <select name="county" id="county" onchange={onChangeCounty}>
-        //             <option value="Alameda">Alameda</option>
-        //             <option value="Amador">Amador</option>
-        //         </select>
-        //     </form>
-        //     <form action='/covid'>
-        //         <label for="type"><b>Type:&nbsp;&nbsp;&nbsp;&nbsp;</b> &nbsp;</label>
-        //         <select name="type" id="type">
-        //             <option value="cases">Cases</option>
-        //             <option value="correlation">Correlation</option>
-        //         </select>
-		// 	</form>
-        // </div>
     )
 }
 

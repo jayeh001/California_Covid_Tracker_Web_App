4f3faf3325c70a1c9fe73e618ca841089df8bb2d
sz125 <sz125@seas.upenn.edu>
2022-12-05 19:35:11 -0800

    not working covid

--- client/cis550-group-project/src/pages/CovidPage.js
+++ client/cis550-group-project/src/pages/CovidPage.js
@@ -1,18 +1,30 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { getCovid } from "../fetcher";
 import { useState } from "react";
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 const CovidPage = () => {
 
-    const [covidData, setCovidData] = useState()
+    const [covidData, setCovidData] = useState();
 
 
     useEffect(() => {
-        getCovid("alameda", "cases").then(res => {
+        getCovid('Alameda', 'cases').then(res => {
             setCovidData(res.results)
         })
     }, [])
 
+
+    function onChangeCounty (event) {
+        var countyName = event.target.value
+        console.log(event.target.value);
+       /* getCovid(countyName, 'cases').then(res => {
+            setCovidData(res.results)
+        })
+        console.log('changing')*/
+    }
 
     console.log(covidData)
 
@@ -20,6 +32,20 @@ const CovidPage = () => {
     return (
         <div>
             <h1>Covid Page</h1>
+            <form action='/covid'>
+                <label for="county"><b>County:</b> &nbsp;</label>
+                <select name="county" id="county" onchange={onChangeCounty}>
+                    <option value="Alameda">Alameda</option>
+                    <option value="Amador">Amador</option>
+                </select>
+            </form>
+            <form action='/covid'>
+                <label for="type"><b>Type:&nbsp;&nbsp;&nbsp;&nbsp;</b> &nbsp;</label>
+                <select name="type" id="type">
+                    <option value="cases">Cases</option>
+                    <option value="correlation">Correlation</option>
+                </select>
+            </form>
         </div>
     )
 }
